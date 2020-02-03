16e343dad0ce1b88b5392dd0d2425a3e280796f9
caetanomb <caetanomb@gmail.com>
2020-02-03 15:05:36 +0100

    setting up

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,8 +1,23 @@
 // Imports
 // TODO: Import the aws-sdk
 const helpers = require('./helpers')
+const dotenv = require('dotenv');
+dotenv.config();
 
-// TODO: Configure region
+const proxyInfo = process.env.PROXY_INFO;
+
+console.log(proxyInfo);
+
+//process.exit();
+
+//Set region
+AWS.config.update({region: 'eu-central-1'})
+AWS.config.loadFromPath('./config.json');
+AWS.config.update({
+  httpOptions: { 
+    agent: proxy(proxyInfo)
+  }
+});
 
 // Declare local variables
 // TODO: Create an ec2 object
@@ -26,9 +41,53 @@ createSecurityGroup(sgName)
 })
 
 // Create functions
-
 function createSecurityGroup (sgName) {
   // TODO: Implement sg creation & setting SSH rule
+  const params = {
+    Description: sgName,
+    GroupName: sgName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createSecurityGroup(params, (err, data) => {
+      if (err) {
+        if (err.code.indexOf('InvalidGroup.Duplicate') < 0) 
+          reject(err)
+      }
+      //else {
+        const params = {
+          GroupId: data.GroupId,
+          IpPermissions: [
+            {
+              IpProtocol: 'tcp',
+              FromPort: 22,
+              ToPort: 22,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            },
+            {
+              IpProtocol: 'tcp',
+              FromPort: 3000,
+              ToPort: 3000,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }
+          ]
+        }
+
+        ec2.authorizeSecurityGroupIngress(params, (err) => {
+          if (err) reject(err)
+          else resolve()
+        })
+     // }
+    })
+  })
 }
 
 function createKeyPair (keyName) {
