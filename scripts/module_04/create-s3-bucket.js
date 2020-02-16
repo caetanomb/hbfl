dd2524c0bdbacd31886fe9e93392d76c3d85f084
Bruno C.M. Caetano <caetano.bruno@tui.nl>
2020-02-16 12:20:12 +0100

    S3 creation and file uploading

--- scripts/module_04/create-s3-bucket.js
+++ scripts/module_04/create-s3-bucket.js
@@ -1,18 +1,35 @@
 // Imports
 const AWS = require('aws-sdk')
+const proxy = require('proxy-agent');
+  const dotenv = require('dotenv'); //it reads system variables .env file
+  dotenv.config();
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+  const proxyInfo = process.env.PROXY_INFO;
+
+  console.log(proxyInfo);
+
+//AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.loadFromPath('./config.json');
 
 // Declare local variables
 // TODO: Create new s3 object
+const s3 = new AWS.S3()
 
-createBucket('hamster-bucket/* TODO: Add a unique identifier */')
+createBucket('hamster-bucket-pluralsight-caetano')
 .then((data) => console.log(data))
 
 function createBucket (bucketName) {
   // TODO: Define params object
+  const params = {
+    Bucket: bucketName,
+    ACL: 'public-read'
+  }
 
   return new Promise((resolve, reject) => {
     // TODO: Create s3 bucket
+    s3.createBucket(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
   })
 }
--- scripts/module_04/upload-s3-objects.js
+++ scripts/module_04/upload-s3-objects.js
@@ -1,12 +1,19 @@
 // Imports
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
+const proxy = require('proxy-agent');
+const dotenv = require('dotenv'); //it reads system variables .env file
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+dotenv.config();
+const proxyInfo = process.env.PROXY_INFO;
+console.log(proxyInfo);
+
+//AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.loadFromPath('./config.json');
 
 // Declare local variables
 const s3 = new AWS.S3()
-const bucketName = '/* TODO: Make this bucket name match what you created */'
+const bucketName = 'hamster-bucket-pluralsight-caetano'
 
 helpers.getPublicFiles()
 .then(files => uploadS3Objects(bucketName, files))
@@ -14,14 +21,25 @@ helpers.getPublicFiles()
 
 function uploadS3Objects (bucketName, files) {
   // TODO: Define putObject params object
+  const params = {
+    Bucket: bucketName,
+    ACL: 'public-read'
+  }
 
   const filePromises = files.map((file) => {
     const newParams = Object.assign({}, params, {
       // TODO: Add individual file params
+      Body: file.contents,
+      Key: file.name,
+      ContentType: helpers.getContentType(file.name)
     })
 
     return new Promise((resolve, reject) => {
       // TODO: Put objects in S3
+      s3.putObject(newParams, (err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      })
     })
   })
 
